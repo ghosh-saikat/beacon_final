843a77372eaa533e560dd8b6ce86efc8845a56dd
ghosh-saikat <2001saikatghosh@gmail.com>
2026-05-15 15:20:08 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -10,4 +10,24 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+  loop: true,
+
+  centeredSlides: true,
+
+  slidesPerView: 3,
+
+  spaceBetween: 10,
+
+  speed: 800,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 3,
+    },
+
+    768: {
+      slidesPerView: 5,
+    },
+  },
 });
